320da704d7a221d4d0247a4067b4310f63b2fc4e
cyberhoward <cyberhoward@protonmail.com>
2024-10-16 11:59:00 +0200

    Initial Commit

---
+++ src/interface.rs
@@ -0,0 +1,46 @@
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cosmwasm_std::Empty;
+use cw_orch::interface;
+use cw_orch::prelude::*;
+
+#[interface(InstantiateMsg, ExecuteMsg, QueryMsg, Empty, id = "buyable-counter")]
+pub struct ContractInterface;
+
+impl<Chain: CwEnv> Uploadable for ContractInterface<Chain> {
+    /// Return the path to the wasm file corresponding to the contract
+    fn wasm(_info: &ChainInfoOwned) -> WasmPath {
+        artifacts_dir_from_workspace!()
+            .find_wasm_path("buyable_counter")
+            .unwrap()
+    }
+    /// Returns a CosmWasm contract wrapper
+    fn wrapper() -> Box<dyn MockContract<Empty>> {
+        Box::new(ContractWrapper::new_with_empty(
+            crate::contract::execute,
+            crate::contract::instantiate,
+            crate::contract::query,
+        ))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::msg::{ExecuteMsgFns, QueryMsgFns};
+    use cw_orch::anyhow;
+
+    #[test]
+    fn contract_logic() -> anyhow::Result<()> {
+        let mock = Mock::new(&Addr::unchecked("sender"));
+        let contract = ContractInterface::new(mock);
+        contract.upload()?;
+
+        contract.instantiate(&InstantiateMsg { count: 7 }, None, &[])?;
+        assert_eq!(contract.get_count()?.count, 7);
+
+        contract.increment()?;
+        assert_eq!(contract.get_count()?.count, 8);
+
+        Ok(())
+    }
+}
